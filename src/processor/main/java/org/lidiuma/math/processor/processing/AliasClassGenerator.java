b061355c7e3a3e9d3c625f714a9bd12e5535046e
xasmedy <xasmedy08cim@gmail.com>
2026-07-22 00:11:22 +0200

    processor: fixed a few issue with the retrieveClass method

--- src/processor/main/java/org/lidiuma/math/processor/processing/AliasClassGenerator.java
+++ src/processor/main/java/org/lidiuma/math/processor/processing/AliasClassGenerator.java
@@ -19,9 +19,7 @@ package org.lidiuma.math.processor.processing;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.TypeKind;
-import javax.lang.model.type.TypeMirror;
+import javax.lang.model.type.*;
 import javax.tools.JavaFileObject;
 import java.io.IOException;
 import java.util.LinkedHashSet;
@@ -32,6 +30,7 @@ public final class AliasClassGenerator {
 
     private static final String CLASS_SOURCE = """
             package %s;
+            %s
             
             public final class %s {
             
@@ -45,6 +44,10 @@ public final class AliasClassGenerator {
                 }\
             """;
     private static final String METHOD_CALL = "%s%s.%s.%s(%s);";
+    private DeclaredType declared;
+    private Utility util;
+    // Class generation information.
+    private final SequencedSet<String> imports = new LinkedHashSet<>();
     private String package_;
     private String class_;
     private SequencedSet<ExecutableElement> methods = new LinkedHashSet<>();
@@ -72,28 +75,32 @@ public final class AliasClassGenerator {
 
     public void build(VariableElement annotated, Utility util) throws IOException {
 
-        Objects.requireNonNull(annotated);
-        Objects.requireNonNull(util);
+        this.util = Objects.requireNonNull(util);
+        this.declared = (DeclaredType) Objects.requireNonNull(annotated).asType();
         if (package_ == null) throw new IllegalArgumentException("The package name is required.");
         if (class_ == null) throw new IllegalArgumentException("The class name is required.");
 
         final StringBuilder methods = new StringBuilder();
         for (var method : this.methods) {
-            methods.append(createMethod(util, annotated, method));
+            methods.append(createMethod(annotated, method));
         }
 
         final String fullName = package_ + "." + class_;
         final JavaFileObject sourceFile = util.processingEnv().getFiler().createSourceFile(fullName);
 
         try (var writer = sourceFile.openWriter()) {
-            final var source = String.format(CLASS_SOURCE, package_, class_, class_, methods);
+            final var source = String.format(CLASS_SOURCE,
+                    package_,
+                    "\n" + String.join("\n", this.imports),
+                    class_,
+                    class_,
+                    methods
+            );
             writer.write(source);
         }
     }
 
-    private String createMethod(Utility util, VariableElement annotated, ExecutableElement method) {
-
-        final var annotatedDeclared = (DeclaredType) annotated.asType();
+    private String createMethod(VariableElement annotated, ExecutableElement method) {
 
         final var returnType = method.getReturnType();
         final var arguments = method.getParameters();
@@ -101,7 +108,7 @@ public final class AliasClassGenerator {
         final StringBuilder argumentsLong = new StringBuilder();
         final StringBuilder argumentsShort = new StringBuilder();
         for (var argument : arguments) {
-            final String type = requiredName(util.specializedOfGeneric(annotatedDeclared, argument.asType()), util);
+            final String type = retrieveClass(argument.asType());
             argumentsLong.append(String.format("%s %s, ", type, argument.getSimpleName()));
             argumentsShort.append(argument.getSimpleName()).append(", ");
         }
@@ -119,27 +126,62 @@ public final class AliasClassGenerator {
                 argumentsShort);
 
         return String.format(METHOD_SOURCE,
-                requiredName(util.specializedOfGeneric(annotatedDeclared, returnType), util),
+                retrieveClass(returnType),
                 method.getSimpleName(),
                 argumentsLong,
                 methodCall);
     }
 
-    /// Retrieves the shortest name that can be used for compilation.
-    private String requiredName(TypeMirror type, Utility util) {
+    /// Imports the necessary classes and returns the class name.
+    private void verifyImports(TypeMirror type) {
 
-        if (type.getKind() != TypeKind.DECLARED) return type.toString();
+        if (type.getKind() != TypeKind.DECLARED) return;
 
         final var elements = util.processingEnv().getElementUtils();
-
         final var element = (TypeElement) ((DeclaredType) type).asElement();
         final String elementPackage = elements.getPackageOf(element).toString();
 
         // Same package, I don't need the full qualified name.
-        if (elementPackage.equals(package_)) return element.getSimpleName().toString();
+        if (elementPackage.equals(package_)) return;
         // Java lang classes are always imported.
-        if (elementPackage.equals("java.lang")) return element.getSimpleName().toString();
+        if (elementPackage.equals("java.lang")) return;
 
-        return element.toString();
+        final String import_ = "import " + element + ";";
+        // I import the missing class.
+        if (elementPackage.startsWith("java")) imports.addFirst(import_);
+        else imports.addLast(import_);
+    }
+
+    private String retrieveClass(TypeMirror type) {
+        final TypeMirror specialized = util.specializedOfGeneric(declared, type);
+        return switch (specialized.getKind()) {
+            case DECLARED -> {
+
+                verifyImports(specialized);
+
+                final var declared = (DeclaredType) specialized;
+                final var name = declared.asElement().getSimpleName().toString();
+                final var sb = new StringBuilder(name);
+
+                final var args = declared.getTypeArguments();
+                if (!args.isEmpty()) {
+                    sb.append("<");
+                    for (int i = 0; i < args.size(); i++) {
+                        if (i > 0) sb.append(", ");
+                        sb.append(retrieveClass(args.get(i)));
+                    }
+                    sb.append(">");
+                }
+                yield sb.toString();
+            }
+            case ARRAY -> retrieveClass(((ArrayType) specialized).getComponentType()) + "[]";
+            case WILDCARD -> {
+                final var wildcard = (WildcardType) specialized;
+                if (wildcard.getExtendsBound() != null) yield "? extends " + retrieveClass(wildcard.getExtendsBound());
+                if (wildcard.getSuperBound() != null) yield "? super " + retrieveClass(wildcard.getSuperBound());
+                yield "?";
+            }
+            default -> specialized.toString();
+        };
     }
 }
