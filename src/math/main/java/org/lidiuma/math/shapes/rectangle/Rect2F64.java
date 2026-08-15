0a5e0370508741d9c0e2faf10cdd28d180fee9ae
xasmedy <xasmedy08cim@gmail.com>
2026-08-15 19:03:13 +0200

    math(shapes): added missing F64 shapes

---
+++ src/math/main/java/org/lidiuma/math/shapes/rectangle/Rect2F64.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2026 Xasmedy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lidiuma.math.shapes.rectangle;
+
+import jdk.internal.vm.annotation.LooselyConsistentValue;
+import jdk.internal.vm.annotation.NullRestricted;
+import org.lidiuma.math.api.shapes.rectangle.Rectangle2;
+import org.lidiuma.math.vector.Vec2F64;
+
+@LooselyConsistentValue
+public value record Rect2F64(@NullRestricted Vec2F64 dimensions) implements Rectangle2<Double> {
+
+    public Rect2F64(double width, double height) {
+        this(new Vec2F64(width, height));
+    }
+}
