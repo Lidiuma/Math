3c3e58740dd422c757861c229f43cbea121a3b33
xasmedy <xasmedy@pm.me>
2026-08-20 22:52:26 +0200

    math(color): added packed rgba, rgba, hex, hsv, hsl factory methods

--- src/math/main/java/org/lidiuma/math/color/ColorF32.java
+++ src/math/main/java/org/lidiuma/math/color/ColorF32.java
@@ -22,7 +22,9 @@ import org.lidiuma.math.api.traits.color.ColorOps;
 import org.lidiuma.math.api.tuple.UnaryTuple4;
 import org.lidiuma.math.processor.FactoryAlias;
 import org.lidiuma.math.processor.FieldAlias;
+import org.lidiuma.math.processor.MethodAlias;
 import org.lidiuma.math.processor.NamedAlias;
+import org.lidiuma.math.rotation.AngleF32;
 import org.lidiuma.math.vector.Vec4F32;
 import java.util.function.UnaryOperator;
 import static org.lidiuma.math.internal.AnnotationConst.*;
@@ -41,6 +43,111 @@ public value record ColorF32(
     @NamedAlias(methodName = COLOR_FACTORY + F32)
     public ColorF32(UnaryTuple4<Float> tuple) {
         this(tuple.x(), tuple.y(), tuple.z(), tuple.w());
+    }
+
+    /// Extracts the `R`, `G`, `B`, and `A` channels from a packed `0xRRGGBBAA` integer.
+    /// @return the extracted colors.
+    @MethodAlias(outputClass = COLOR_OUT)
+    @NamedAlias(methodName = COLOR_FACTORY + F32 + "Rgba")
+    public static ColorF32 colorRgba(int rgba) {
+        return colorRgba(
+                rgba >>> 24,
+                rgba >>> 16 & 0xFF,
+                rgba >>> 8 & 0xFF,
+                rgba & 0xFF
+        );
+    }
+
+    /// Converts the color channels from an int value in the range `[0,255]` to a float value in the range `[0,1]`.\
+    /// Providing integers above `255` is allowed, but the resulting value will be above `1`.
+    /// @return the color with the channels normalized by dividing each value by `255`.
+    @MethodAlias(outputClass = COLOR_OUT)
+    @NamedAlias(methodName = COLOR_FACTORY + F32 + "Rgba")
+    public static ColorF32 colorRgba(int red, int green, int blue, int alpha) {
+        return new ColorF32(
+                  red / 255f,
+                green / 255f,
+                 blue / 255f,
+                alpha / 255f
+        );
+    }
+
+    /// Converts a hex string to a color.\
+    /// The hex can contain `#`, which will simply be ignored,
+    ///  and it must provide either `RRGGBB` or `RRGGBBAA` in hexadecimal format.
+    @MethodAlias(outputClass = COLOR_OUT)
+    @NamedAlias(methodName = COLOR_FACTORY + F32 + "Hex")
+    public static ColorF32 colorHex(String hexColor) {
+        final String hex = hexColor.startsWith("#") ? hexColor.substring(1) : hexColor;
+        final int length = hex.length();
+        if (length != 6 && length != 8) throw new IllegalArgumentException("The hex length can either be 6 or 8, provided: " + hex + ".");
+        return colorRgba(Integer.parseUnsignedInt(hex + (length == 6 ? "FF" : ""), 16));
+    }
+
+    /// Converts the HSV to a color.
+    /// @param hue the angle of the hue, any angle is allowed.
+    /// @param saturation the saturation in the range [0,1].
+    /// @param value the value in the range [0,1].
+    /// @return the converted color.
+    @MethodAlias(outputClass = COLOR_OUT)
+    @NamedAlias(methodName = COLOR_FACTORY + F32 + "Hsv")
+    public static ColorF32 colorHsv(AngleF32 hue, float saturation, float value) {
+
+        if (value == 0f) return new ColorF32(0f, 0f, 0f, 1f); // Black.
+        if (saturation == 0f) return new ColorF32(value, value, value, 1f); // Grayscale.
+
+        // Normalizes hue into one of 6 zones.
+        final float x = (float) (AngleF32.normalize(hue).radian() / (Math.TAU / 6f));
+        final int zone = (int) x;
+        final float remainder = x - zone;
+
+        final float p = value * (1f - saturation);
+        final float q = value * (1f - saturation * remainder);
+        final float t = value * (1f - saturation * (1f - remainder));
+
+        return switch (zone) {
+            case 0 -> new ColorF32(value, t, p, 1f); // Red.
+            case 1 -> new ColorF32(q, value, p, 1f); // Yellow.
+            case 2 -> new ColorF32(p, value, t, 1f); // Green.
+            case 3 -> new ColorF32(p, q, value, 1f); // Cyan.
+            case 4 -> new ColorF32(t, p, value, 1f); // Blue.
+            case 5 -> new ColorF32(value, p, q, 1f); // Magenta.
+            default -> throw new AssertionError("Illegal zone.");
+        };
+    }
+
+    /// Converts the HSL to a color.
+    /// @param hue the angle of the hue, any angle is allowed.
+    /// @param saturation the saturation in the range [0,1].
+    /// @param lightness the lightness in the range [0,1].
+    /// @return the converted color.
+    @MethodAlias(outputClass = COLOR_OUT)
+    @NamedAlias(methodName = COLOR_FACTORY + F32 + "Hsl")
+    public static ColorF32 colorHsl(AngleF32 hue, float saturation, float lightness) {
+
+        if (lightness == 0f) return new ColorF32(0f, 0f, 0f, 1f); // Black.
+        if (saturation == 0f) return new ColorF32(lightness, lightness, lightness, 1f); // Grayscale.
+
+        // Normalizes hue into one of 6 zones.
+        final float x = (float) (AngleF32.normalize(hue).radian() / (Math.TAU / 6f));
+        final int zone = (int) x;
+        final float remainder = x - zone;
+
+        final float chroma = (1f - Math.abs(2f * lightness - 1f)) * saturation;
+        final float min = lightness - chroma / 2f;
+        final float max = lightness + chroma / 2f;
+        final float q = max - chroma * remainder;
+        final float t = min + chroma * remainder;
+
+        return switch (zone) {
+            case 0 -> new ColorF32(max, t, min, 1f); // Red.
+            case 1 -> new ColorF32(q, max, min, 1f); // Yellow.
+            case 2 -> new ColorF32(min, max, t, 1f); // Green.
+            case 3 -> new ColorF32(min, q, max, 1f); // Cyan.
+            case 4 -> new ColorF32(t, min, max, 1f); // Blue.
+            case 5 -> new ColorF32(max, min, q, 1f); // Magenta.
+            default -> throw new AssertionError("Illegal zone.");
+        };
     }
 
     public static final value class Ops implements ColorOps<ColorF32, Float> {
--- src/math/main/java/org/lidiuma/math/color/ColorF64.java
+++ src/math/main/java/org/lidiuma/math/color/ColorF64.java
@@ -22,7 +22,9 @@ import org.lidiuma.math.api.traits.color.ColorOps;
 import org.lidiuma.math.api.tuple.UnaryTuple4;
 import org.lidiuma.math.processor.FactoryAlias;
 import org.lidiuma.math.processor.FieldAlias;
+import org.lidiuma.math.processor.MethodAlias;
 import org.lidiuma.math.processor.NamedAlias;
+import org.lidiuma.math.rotation.AngleF64;
 import org.lidiuma.math.vector.Vec4F64;
 import java.util.function.UnaryOperator;
 import static org.lidiuma.math.internal.AnnotationConst.*;
@@ -41,6 +43,111 @@ public value record ColorF64(
     @NamedAlias(methodName = COLOR_FACTORY + F64)
     public ColorF64(UnaryTuple4<Double> tuple) {
         this(tuple.x(), tuple.y(), tuple.z(), tuple.w());
+    }
+
+    /// Extracts the `R`, `G`, `B`, and `A` channels from a packed `0xRRGGBBAA` integer.
+    /// @return the extracted colors.
+    @MethodAlias(outputClass = COLOR_OUT)
+    @NamedAlias(methodName = COLOR_FACTORY + F64 + "Rgba")
+    public static ColorF64 colorRgba(int rgba) {
+        return colorRgba(
+                rgba >>> 24,
+                rgba >>> 16 & 0xFF,
+                rgba >>> 8 & 0xFF,
+                rgba & 0xFF
+        );
+    }
+
+    /// Converts the color channels from an int value in the range `[0,255]` to a double value in the range `[0,1]`.\
+    /// Providing integers above `255` is allowed, but the resulting value will be above `1`.
+    /// @return the color with the channels normalized by dividing each value by `255`.
+    @MethodAlias(outputClass = COLOR_OUT)
+    @NamedAlias(methodName = COLOR_FACTORY + F64 + "Rgba")
+    public static ColorF64 colorRgba(int red, int green, int blue, int alpha) {
+        return new ColorF64(
+                red / 255d,
+                green / 255d,
+                blue / 255d,
+                alpha / 255d
+        );
+    }
+
+    /// Converts a hex string to a color.\
+    /// The hex can contain `#`, which will simply be ignored,
+    ///  and it must provide either `RRGGBB` or `RRGGBBAA` in hexadecimal format.
+    @MethodAlias(outputClass = COLOR_OUT)
+    @NamedAlias(methodName = COLOR_FACTORY + F64 + "Hex")
+    public static ColorF64 colorHex(String hexColor) {
+        final String hex = hexColor.startsWith("#") ? hexColor.substring(1) : hexColor;
+        final int length = hex.length();
+        if (length != 6 && length != 8) throw new IllegalArgumentException("The hex length can either be 6 or 8, provided: " + hex + ".");
+        return colorRgba(Integer.parseUnsignedInt(hex + (length == 6 ? "FF" : ""), 16));
+    }
+
+    /// Converts the HSV to a color.
+    /// @param hue the angle of the hue, any angle is allowed.
+    /// @param saturation the saturation in the range [0,1].
+    /// @param value the value in the range [0,1].
+    /// @return the converted color.
+    @MethodAlias(outputClass = COLOR_OUT)
+    @NamedAlias(methodName = COLOR_FACTORY + F64 + "Hsv")
+    public static ColorF64 colorHsv(AngleF64 hue, double saturation, double value) {
+
+        if (value == 0d) return new ColorF64(0d, 0d, 0d, 1d); // Black.
+        if (saturation == 0d) return new ColorF64(value, value, value, 1d); // Grayscale.
+
+        // Normalizes hue into one of 6 zones.
+        final double x = AngleF64.normalize(hue).radian() / (Math.TAU / 6d);
+        final int zone = (int) x;
+        final double remainder = x - zone;
+
+        final double p = value * (1d - saturation);
+        final double q = value * (1d - saturation * remainder);
+        final double t = value * (1d - saturation * (1d - remainder));
+
+        return switch (zone) {
+            case 0 -> new ColorF64(value, t, p, 1d); // Red.
+            case 1 -> new ColorF64(q, value, p, 1d); // Yellow.
+            case 2 -> new ColorF64(p, value, t, 1d); // Green.
+            case 3 -> new ColorF64(p, q, value, 1d); // Cyan.
+            case 4 -> new ColorF64(t, p, value, 1d); // Blue.
+            case 5 -> new ColorF64(value, p, q, 1d); // Magenta.
+            default -> throw new AssertionError("Illegal zone.");
+        };
+    }
+
+    /// Converts the HSL to a color.
+    /// @param hue the angle of the hue, any angle is allowed.
+    /// @param saturation the saturation in the range [0,1].
+    /// @param lightness the lightness in the range [0,1].
+    /// @return the converted color.
+    @MethodAlias(outputClass = COLOR_OUT)
+    @NamedAlias(methodName = COLOR_FACTORY + F64 + "Hsl")
+    public static ColorF64 colorHsl(AngleF64 hue, double saturation, double lightness) {
+
+        if (lightness == 0d) return new ColorF64(0d, 0d, 0d, 1d); // Black.
+        if (saturation == 0d) return new ColorF64(lightness, lightness, lightness, 1d); // Grayscale.
+
+        // Normalizes hue into one of 6 zones.
+        final double x = AngleF64.normalize(hue).radian() / (Math.TAU / 6d);
+        final int zone = (int) x;
+        final double remainder = x - zone;
+
+        final double chroma = (1d - Math.abs(2d * lightness - 1d)) * saturation;
+        final double min = lightness - chroma / 2d;
+        final double max = lightness + chroma / 2d;
+        final double q = max - chroma * remainder;
+        final double t = min + chroma * remainder;
+
+        return switch (zone) {
+            case 0 -> new ColorF64(max, t, min, 1d); // Red.
+            case 1 -> new ColorF64(q, max, min, 1d); // Yellow.
+            case 2 -> new ColorF64(min, max, t, 1d); // Green.
+            case 3 -> new ColorF64(min, q, max, 1d); // Cyan.
+            case 4 -> new ColorF64(t, min, max, 1d); // Blue.
+            case 5 -> new ColorF64(max, min, q, 1d); // Magenta.
+            default -> throw new AssertionError("Illegal zone.");
+        };
     }
 
     public static final value class Ops implements ColorOps<ColorF64, Double> {
