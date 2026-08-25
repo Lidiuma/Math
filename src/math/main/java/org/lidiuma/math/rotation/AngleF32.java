80d42cf0bc92e5f3f4d27e6d2c88137c6c5bc8f1
xasmedy <xasmedy@pm.me>
2026-08-25 13:51:36 +0200

    math(rotation): added AngleOps implementations

--- src/math/main/java/org/lidiuma/math/rotation/AngleF32.java
+++ src/math/main/java/org/lidiuma/math/rotation/AngleF32.java
@@ -17,57 +17,143 @@
 package org.lidiuma.math.rotation;
 
 import jdk.internal.vm.annotation.LooselyConsistentValue;
-import jdk.internal.vm.annotation.NullRestricted;
 import org.lidiuma.math.api.rotation.Angle;
-import org.lidiuma.math.processor.AliasExclude;
+import org.lidiuma.math.api.traits.rotation.AngleOps;
+import org.lidiuma.math.internal.Epsilon;
 import org.lidiuma.math.processor.FactoryAlias;
-import org.lidiuma.math.processor.MethodAlias;
+import org.lidiuma.math.processor.FieldAlias;
+import org.lidiuma.math.processor.NamedAlias;
+import org.lidiuma.math.vector.Vec2F32;
+import java.util.function.UnaryOperator;
 import static org.lidiuma.math.internal.AnnotationConst.*;
 
 @FactoryAlias(methodName = ANGLE_FACTORY + F32, outputClass = ROTATION_OUT)
 @LooselyConsistentValue
-public value record AngleF32(@Override @NullRestricted Float radian) implements Angle<Float> {
+public value class AngleF32 implements Angle<Float> {
 
-    @AliasExclude
-    public AngleF32 {}
+    @FieldAlias(outputClass = ROTATION_OUT)
+    public static final Ops OPS = new Ops();
+
+    // Hidden representation since this class represent a generic angle and not what it's made of.
+    private float radians;
+
+    private AngleF32(float radians) {
+        this.radians = radians;
+    }
 
     public AngleF32(Angle<Float> angle) {
-        this(angle.radian());
-    }
-
-    @MethodAlias(outputClass = ROTATION_OUT)
-    public static AngleF32 radians(float radians) {
-        return new AngleF32(radians);
-    }
-
-    @MethodAlias(outputClass = ROTATION_OUT)
-    public static AngleF32 degrees(float degrees) {
-        return new AngleF32((float) Math.toRadians(degrees));
-    }
-
-    @MethodAlias(outputClass = ROTATION_OUT)
-    public static AngleF32 turns(float turns) {
-        return new AngleF32((float) (turns * Math.TAU));
-    }
-
-    /// Normalizes an angle into the range `[0,2π)`.
-    /// Negative angles are wrapped into the positive range.
-    /// @param angle the angle to normalize.
-    /// @return the normalized angle.
-    @MethodAlias(outputClass = ROTATION_OUT)
-    public static AngleF32 normalize(AngleF32 angle) {
-        final float normalized = (float) (angle.radian() % Math.TAU);
-        final float radians = (float) (normalized + (normalized < 0f ? Math.TAU : 0f));
-        return new AngleF32(radians);
+        this(angle.radians());
     }
 
     @Override
-    public Float degree() {
-        return (float) Math.toDegrees(radian);
+    public Float radians() {
+        return radians;
     }
 
     @Override
-    public Float turn() {
-        return (float) (radian / Math.TAU);
+    public Float degrees() {
+        return (float) Math.toDegrees(radians());
+    }
+
+    @Override
+    public Float turns() {
+        return (float) (radians() / Math.TAU);
+    }
+
+    @Override
+    public Vec2F32 vector() {
+        return new Vec2F32(
+                Rotations.cos(this),
+                Rotations.sin(this)
+        );
+    }
+
+    public static final value class Ops implements AngleOps<AngleF32, Vec2F32, Float> {
+
+        @Override
+        @NamedAlias(methodName = "radians")
+        public AngleF32 fromRadians(Float radians) {
+            return new AngleF32(radians);
+        }
+
+        @Override
+        @NamedAlias(methodName = "degrees")
+        public AngleF32 fromDegrees(Float degrees) {
+            return fromRadians((float) Math.toRadians(degrees));
+        }
+
+        @Override
+        @NamedAlias(methodName = "turns")
+        public AngleF32 fromTurns(Float turns) {
+            return fromRadians((float) (turns * Math.TAU));
+        }
+
+        @Override
+        @NamedAlias(methodName = "vectorAngle")
+        public AngleF32 fromVector(Vec2F32 vector) {
+            final var angle = (float) Math.atan2(vector.y(), vector.x());
+            return fromRadians(angle < 0f ? (float) (angle + Math.TAU) : angle);
+        }
+
+        @Override
+        public Float cos(AngleF32 angle) {
+            // If I cast after the cos(), the cast loses precision and goes bellow epsilon.
+            return Epsilon.clamp((float) Math.cos(angle.radians()));
+        }
+
+        @Override
+        public Float sin(AngleF32 angle) {
+            // If I cast after the sin(), the cast loses precision and goes bellow epsilon.
+            return Epsilon.clamp((float) Math.sin(angle.radians()));
+        }
+
+        @Override
+        public Float tan(AngleF32 angle) {
+            return (float) Math.tan(angle.radians());
+        }
+
+        @Override
+        public AngleF32 normalize(AngleF32 angle) {
+            final float normalized = (float) (angle.radians() % Math.TAU);
+            return fromRadians((float) (normalized + (normalized < 0f ? Math.TAU : 0f)));
+        }
+
+        @Override
+        public AngleF32 add(AngleF32 op1, AngleF32 op2) {
+            return fromRadians(op1.radians() + op2.radians());
+        }
+
+        @Override
+        public AngleF32 subtract(AngleF32 op1, AngleF32 op2) {
+            return fromRadians(op1.radians() - op2.radians());
+        }
+
+        @Override
+        public AngleF32 multiply(AngleF32 op1, Float scalar) {
+            return fromRadians(op1.radians() * scalar);
+        }
+
+        @Override
+        public AngleF32 divide(AngleF32 op1, Float scalar) {
+            return fromRadians(op1.radians() / scalar);
+        }
+
+        @Override
+        public AngleF32 negated(AngleF32 operand) {
+            return normalize(fromRadians(-operand.radians()));
+        }
+
+        @Override
+        public AngleF32 interpolate(AngleF32 start, AngleF32 end, Float alpha, UnaryOperator<Float> easing) {
+
+            final float easedAlpha = easing.apply(alpha);
+            final float startRadian = start.radians();
+
+            // Compute the shortest angular difference
+            final float delta = end.radians() - startRadian;
+            final float shortestDelta = (float) (delta - Math.TAU * Math.floor((delta + Math.PI) / Math.TAU));
+
+            return normalize(fromRadians(startRadian + shortestDelta * easedAlpha));
+        }
     }
 }
--- src/math/main/java/org/lidiuma/math/rotation/AngleF64.java
+++ src/math/main/java/org/lidiuma/math/rotation/AngleF64.java
@@ -17,57 +17,141 @@
 package org.lidiuma.math.rotation;
 
 import jdk.internal.vm.annotation.LooselyConsistentValue;
-import jdk.internal.vm.annotation.NullRestricted;
 import org.lidiuma.math.api.rotation.Angle;
-import org.lidiuma.math.processor.AliasExclude;
+import org.lidiuma.math.api.traits.rotation.AngleOps;
+import org.lidiuma.math.internal.Epsilon;
 import org.lidiuma.math.processor.FactoryAlias;
-import org.lidiuma.math.processor.MethodAlias;
+import org.lidiuma.math.processor.FieldAlias;
+import org.lidiuma.math.processor.NamedAlias;
+import org.lidiuma.math.vector.Vec2F64;
+import java.util.function.UnaryOperator;
 import static org.lidiuma.math.internal.AnnotationConst.*;
 
 @FactoryAlias(methodName = ANGLE_FACTORY + F64, outputClass = ROTATION_OUT)
 @LooselyConsistentValue
-public value record AngleF64(@Override @NullRestricted Double radian) implements Angle<Double> {
+public value class AngleF64 implements Angle<Double> {
 
-    @AliasExclude
-    public AngleF64 {}
+    @FieldAlias(outputClass = ROTATION_OUT)
+    public static final Ops OPS = new Ops();
+
+    // Hidden representation since this class represent a generic angle and not what it's made of.
+    private double radians;
+
+    private AngleF64(double radians) {
+        this.radians = radians;
+    }
 
     public AngleF64(Angle<Double> angle) {
-        this(angle.radian());
-    }
-
-    @MethodAlias(outputClass = ROTATION_OUT)
-    public static AngleF64 radians(double radians) {
-        return new AngleF64(radians);
-    }
-
-    @MethodAlias(outputClass = ROTATION_OUT)
-    public static AngleF64 degrees(double degrees) {
-        return new AngleF64(Math.toRadians(degrees));
-    }
-
-    @MethodAlias(outputClass = ROTATION_OUT)
-    public static AngleF64 turns(double turns) {
-        return new AngleF64(turns * Math.TAU);
-    }
-
-    /// Normalizes an angle into the range `[0,2π)`.
-    /// Negative angles are wrapped into the positive range.
-    /// @param angle the angle to normalize.
-    /// @return the normalized angle.
-    @MethodAlias(outputClass = ROTATION_OUT)
-    public static AngleF64 normalize(AngleF64 angle) {
-        final double normalized = angle.radian() % Math.TAU;
-        final double radians = normalized + (normalized < 0d ? Math.TAU : 0d);
-        return new AngleF64(radians);
+        this(angle.radians());
     }
 
     @Override
-    public Double degree() {
-        return Math.toDegrees(radian);
+    public Double radians() {
+        return radians;
     }
 
     @Override
-    public Double turn() {
-        return radian / Math.TAU;
+    public Double degrees() {
+        return Math.toDegrees(radians());
+    }
+
+    @Override
+    public Double turns() {
+        return radians() / Math.TAU;
+    }
+
+    @Override
+    public Vec2F64 vector() {
+        return new Vec2F64(
+                Rotations.cos(this),
+                Rotations.sin(this)
+        );
+    }
+
+    public static final value class Ops implements AngleOps<AngleF64, Vec2F64, Double> {
+
+        @Override
+        @NamedAlias(methodName = "radians")
+        public AngleF64 fromRadians(Double radians) {
+            return new AngleF64(radians);
+        }
+
+        @Override
+        @NamedAlias(methodName = "degrees")
+        public AngleF64 fromDegrees(Double degrees) {
+            return fromRadians(Math.toRadians(degrees));
+        }
+
+        @Override
+        @NamedAlias(methodName = "turns")
+        public AngleF64 fromTurns(Double turns) {
+            return fromRadians((turns * Math.TAU));
+        }
+
+        @Override
+        @NamedAlias(methodName = "vectorAngle")
+        public AngleF64 fromVector(Vec2F64 vector) {
+            final var angle = Math.atan2(vector.y(), vector.x());
+            return fromRadians(angle < 0d ? (angle + Math.TAU) : angle);
+        }
+
+        @Override
+        public Double cos(AngleF64 angle) {
+            return Epsilon.clamp(Math.cos(angle.radians()));
+        }
+
+        @Override
+        public Double sin(AngleF64 angle) {
+            return Epsilon.clamp(Math.sin(angle.radians()));
+        }
+
+        @Override
+        public Double tan(AngleF64 angle) {
+            return Math.tan(angle.radians());
+        }
+
+        @Override
+        public AngleF64 normalize(AngleF64 angle) {
+            final double normalized = angle.radians() % Math.TAU;
+            return fromRadians(normalized + (normalized < 0d ? Math.TAU : 0d));
+        }
+
+        @Override
+        public AngleF64 add(AngleF64 op1, AngleF64 op2) {
+            return fromRadians(op1.radians() + op2.radians());
+        }
+
+        @Override
+        public AngleF64 subtract(AngleF64 op1, AngleF64 op2) {
+            return fromRadians(op1.radians() - op2.radians());
+        }
+
+        @Override
+        public AngleF64 multiply(AngleF64 op1, Double scalar) {
+            return fromRadians(op1.radians() * scalar);
+        }
+
+        @Override
+        public AngleF64 divide(AngleF64 op1, Double scalar) {
+            return fromRadians(op1.radians() / scalar);
+        }
+
+        @Override
+        public AngleF64 negated(AngleF64 operand) {
+            return normalize(fromRadians(-operand.radians()));
+        }
+
+        @Override
+        public AngleF64 interpolate(AngleF64 start, AngleF64 end, Double alpha, UnaryOperator<Double> easing) {
+
+            final double easedAlpha = easing.apply(alpha);
+            final double startRadian = start.radians();
+
+            // Compute the shortest angular difference
+            final double delta = end.radians() - startRadian;
+            final double shortestDelta = delta - Math.TAU * Math.floor((delta + Math.PI) / Math.TAU);
+
+            return normalize(fromRadians(startRadian + shortestDelta * easedAlpha));
+        }
     }
 }
